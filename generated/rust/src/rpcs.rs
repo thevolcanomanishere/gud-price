3d39046ec3309f1a447c10e2ad893275eff65ac5
A McG <9613565+thevolcanomanishere@users.noreply.github.com>
2026-03-27 09:57:26 +0000

    fix: reorder rpcs to work in git workflow

--- generated/rust/src/rpcs.rs
+++ generated/rust/src/rpcs.rs
@@ -5,8 +5,8 @@ pub fn rpc(chain: &str) -> &'static str {
     match chain {
         "ethereum" => "https://gateway.tenderly.co/public/mainnet",
         "polygon" => "https://polygon.gateway.tenderly.co",
-        "arbitrum" => "https://arbitrum-one-rpc.publicnode.com",
-        "base" => "https://base-rpc.publicnode.com",
+        "arbitrum" => "https://arbitrum.drpc.org",
+        "base" => "https://base.drpc.org",
         "optimism" => "https://go.getblock.io/e8a75f8dcf614861becfbcb185be6eb4",
         "avalanche" => "https://api.avax.network/ext/bc/C/rpc",
         "bsc" => "https://rpc.sentio.xyz/bsc",
@@ -30,8 +30,8 @@ pub fn rpcs(chain: &str) -> &'static [&'static str] {
     match chain {
         "ethereum" => &["https://gateway.tenderly.co/public/mainnet", "https://eth-mainnet.rpcfast.com?api_key=xbhWBI1Wkguk8SNMu1bvvLurPGLXmgwYeC4S6g2H7WdwFigZSmPWVZRxrskEQwIf", "https://cloudflare-eth.com", "https://ethereum-rpc.publicnode.com", "https://virtual.mainnet.rpc.tenderly.co/5804dcf7-70e6-4988-b2b0-3672193e0c91", "https://mainnet.gateway.tenderly.co"],
         "polygon" => &["https://polygon.gateway.tenderly.co", "https://polygon-bor-rpc.publicnode.com", "https://polygon.drpc.org", "https://gateway.tenderly.co/public/polygon", "https://polygon-public.nodies.app", "https://polygon-mainnet.rpcfast.com?api_key=xbhWBI1Wkguk8SNMu1bvvLurPGLXmgwYeC4S6g2H7WdwFigZSmPWVZRxrskEQwIf"],
-        "arbitrum" => &["https://arbitrum-one-rpc.publicnode.com", "https://arbitrum.drpc.org", "https://arbitrum-one-public.nodies.app", "https://arbitrum.public.blockpi.network/v1/rpc/public", "https://arbitrum.meowrpc.com", "https://arbitrum-one.public.blastapi.io"],
-        "base" => &["https://base-rpc.publicnode.com", "https://base-public.nodies.app", "https://base.meowrpc.com", "https://base.llamarpc.com", "https://base.drpc.org", "https://base-mainnet.public.blastapi.io"],
+        "arbitrum" => &["https://arbitrum.drpc.org", "https://arbitrum-one.public.blastapi.io", "https://arbitrum-one-public.nodies.app", "https://arbitrum.public.blockpi.network/v1/rpc/public", "https://arbitrum.meowrpc.com", "https://arbitrum-one-rpc.publicnode.com"],
+        "base" => &["https://base.drpc.org", "https://base-mainnet.public.blastapi.io", "https://base.llamarpc.com", "https://base-public.nodies.app", "https://base.meowrpc.com", "https://base-rpc.publicnode.com"],
         "optimism" => &["https://go.getblock.io/e8a75f8dcf614861becfbcb185be6eb4", "https://optimism-rpc.publicnode.com", "https://optimism.drpc.org", "https://gateway.tenderly.co/public/optimism", "https://optimism.gateway.tenderly.co", "https://mainnet.optimism.io"],
         "avalanche" => &["https://api.avax.network/ext/bc/C/rpc", "https://avalanche-c-chain-rpc.publicnode.com", "https://avalanche-public.nodies.app/ext/bc/C/rpc", "https://avalanche-mainnet.gateway.tenderly.co", "https://avalanche.api.onfinality.io/public/ext/bc/C/rpc", "https://1rpc.io/avax/c"],
         "bsc" => &["https://rpc.sentio.xyz/bsc", "https://bsc-mainnet.rpcfast.com?api_key=xbhWBI1Wkguk8SNMu1bvvLurPGLXmgwYeC4S6g2H7WdwFigZSmPWVZRxrskEQwIf", "https://bsc.rpc.blxrbdn.com", "https://bnb.rpc.subquery.network/public", "https://bsc-mainnet.nodereal.io/v1/64a9df0874fb4a93b9d0a3849de012d3", "https://binance.nodereal.io"],
